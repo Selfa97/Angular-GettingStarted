55f9330c21071bb6894a2d3583150d9b99ba601d
Sam Fairs <selfa97@outlook.com>
2022-01-25 22:13:53 +1100

    Added products service with DI. Added HTTP request to retrieve products (fetches from JSON file locally).

---
+++ APM-Start/src/app/products/product.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators"
+import { IProduct } from "./product";
+
+@Injectable({
+    providedIn: "root"
+})
+export class ProductService {
+    private productsURL = "api/products/products.json";
+
+    constructor(private http: HttpClient) {}
+
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productsURL).pipe(
+            tap(data => console.log("All", JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        let errorMessage = "";
+        // Client-side or network error occured
+        if (err.error instanceof ErrorEvent) {
+            errorMessage = `An error occured: ${err.error.message}`;
+        } 
+        // Backed returned unsuccessful response code
+        else {
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.log(errorMessage);
+        return throwError(errorMessage);
+    }
+}
